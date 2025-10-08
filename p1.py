1f395b7a8035e8a08176d3e345189bc147c9eedf
anitha44 <anithathayyuru9100@gmail.com>
2025-10-08 11:32:55 +0530

    assement

---
+++ p1.py
@@ -0,0 +1,18 @@
+from turtle import *
+speed('fast')
+pencolor('red')
+pensize(3)
+
+side = 6
+for i in range(side):
+       fd(50)
+       lt(360/side)
+       fd(100)
+       it(360/side)
+
+hideturtle()
+mainloop()
+
+
+import keyword
+print(keyword.kwlist)
